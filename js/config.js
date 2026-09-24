c97887bb08079b714037e05f4831ffa1a309ff82
mathhsilva <mathcsilva97@gmail.com>
2026-09-24 09:56:13 -0300

    Ajuste do numero de telefone para conclusão de pedido

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@
  */
 window.LOJA = {
   nome: "mstag",
-  // Número com DDI + DDD, só dígitos. Ex.: 5511987654321
-  whatsapp: "5511999999999",
+  // Número com DDI + DDD, só dígitos. Ex.: 5541984699726
+  whatsapp: "5541984699726",
   instagram: "mstagbr",
   email: "contato@mstag.com.br",
   cidade: "Enviamos para todo o Brasil",
